1c9c737e0b22eee19d31f2ad3702c244f996fca0
Zoolbazuzu <zool81@mail.ru>
2026-09-05 00:13:08 +0300

    Initial commit

---
+++ powerm3/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'powerm3-v5';
+const ASSETS = ['./index.html', './manifest.json', './sw.js'];
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting();
+});
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+    self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((cached) => {
+        const fp = fetch(e.request).then((resp) => {
+            if (resp && resp.status === 200) { const clone = resp.clone(); caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone)); }
+            return resp;
+        }).catch(() => cached);
+        return cached || fp;
+    }));
+});
